623569d2a11efa1b867384fc27d3561241717f0c
Vinay Sharma <vinay04sharma@icloud.com>
2018-08-03 13:11:17 +0530

    msg

--- DeepLearningSuite/package.sh
+++ DeepLearningSuite/package.sh
@@ -1,60 +1,64 @@
+#!/usr/bin/env bash
+
 ########################################################################
 # Package the binaries built on Travis-CI as an AppImage
 # By Simon Peter 2016
 # For more information, see http://appimage.org/
 ########################################################################
 
-
-PREFIX=app
-
 export ARCH=$(arch)
 
-export APPIMAGEBASE=$(pwd)
-
-APP=photoflow
+APP=Pharo
 LOWERAPP=${APP,,}
 
+
+mkdir -p $APP.AppDir/usr/
 wget -q https://github.com/probonopd/AppImages/raw/master/functions.sh -O ./functions.sh
 . ./functions.sh
 
-pwd
+cd $APP.AppDir
 
-#cd $APP.AppDir
+wget -O - get.pharo.org/vm60 | bash
 
-#transfer $APP.AppDir.tgz
-#echo ""
-#transfer $APP.tgz
-#echo ""
+mkdir -p usr/bin
+mv pharo-vm/pharo usr/bin/pharo
 
-mkdir -p AppImage && cd AppImage
-tar xzf ../$APP.AppDir.tgz
-ls -lh
-echo "sudo chown -R $USER $APP.AppDir"
-sudo chown -R $USER $APP.AppDir
-ls -lh
-export APPDIR=$(pwd)/$APP.AppDir
+mkdir -p usr/lib
+mv pharo-vm/* usr/lib
 
-########################################################################
-# Determine the version of the app; also include needed glibc version
-########################################################################
+cd usr/ ; find . -type f -exec sed -i -e 's|/usr|././|g' {} \; ; cd -
 
-GLIBC_NEEDED=$(glibc_needed)
-VERSION=git-${TRAVIS_BRANCH}-gtk3-$(date +%Y%m%d)_$(date +%H%M)-${TRAVIS_COMMIT}.glibc${GLIBC_NEEDED}
-#VERSION=${RELEASE_VERSION}-glibc$GLIBC_NEEDED
+rm -rf pharo pharo-ui pharo-vm
 
+cat > AppRun << 'EOF'
+#!/usr/bin/env bash
+# some magic to find out the real location of this script dealing with symlinks
+DIR=`readlink "$0"` || DIR="$0";
+DIR=`dirname "$DIR"`;
+cd "$DIR"
+DIR=`pwd`
+cd - > /dev/null
+# disable parameter expansion to forward all arguments unprocessed to the VM
+set -f
+# run the VM and pass along all arguments as is
+LD_LIBRARY_PATH="$DIR/usr/lib" "${DIR}/usr/bin/pharo" `zenity --file-selection --filename="$PWD"` "$@"
+EOF
 
-mkdir -p ../out/
-ARCH="x86_64"
-generate_appimage
-#generate_type2_appimage
+chmod +x AppRun
 
-pwd
-ls ../out/*
+wget http://files.pharo.org/media/logo/icon-lighthouse-512x512.png -O $APP.png
 
-########################################################################
-# Upload the AppDir
-########################################################################
+cat > $APP.desktop <<EOF
+[Desktop Entry]
+Name=$APP
+Icon=$APP
+Exec=AppRun
+Categories=Programming;
+EOF
 
-transfer ../out/*
-echo ""
-echo "AppImage has been uploaded to the URL above; use something like GitHub Releases for permanent storage"
+cd ..
+
+wget "https://github.com/probonopd/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+chmod a+x appimagetool-x86_64.AppImage
+
+./appimagetool-x86_64.AppImage Pharo.AppDir
