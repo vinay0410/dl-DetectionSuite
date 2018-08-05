6cfbfaa46b3cf5d34f1386752949d3e3cf7988eb
Vinay Sharma <vinay04sharma@icloud.com>
2018-08-05 07:33:34 +0000

    osx qt

--- install_opencv.sh
+++ install_opencv.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
-set -ex
-wget https://github.com/opencv/opencv/archive/3.4.2.zip
-unzip -q 3.4.2.zip
-cd opencv-3.4.2
-mkdir build && cd build
-cmake ..
-make -j4
-sudo make install
+if [ -n "$(ls -A opencv/build)" ];
+ then
+ 	# We're using a cached version of our OpenCV build
+ 	cd opencv/build;
+	sudo make install
+ else
+ 	# No OpenCV cache – clone and make the files
+ 	rm -rf opencv;
+	wget https://github.com/opencv/opencv/archive/3.4.2.zip
+	unzip -q 3.4.2.zip
+	cd opencv-3.4.2
+	mkdir build && cd build
+	cmake ..
+	make -j4
+	sudo make install
+ fi
+
