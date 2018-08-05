de5ce40b5e8b6d38f0ebdb4f6906417bca0fb424
Vinay Sharma <vinay04sharma@icloud.com>
2018-08-05 09:44:00 +0000

    fixed categories

--- DeepLearningSuite/package.sh
+++ DeepLearningSuite/package.sh
@@ -52,13 +52,15 @@ Name=$APP
 Icon=$APP
 Exec=AppRun
 Type=Application
-Categories=Utilities;
+Categories=Education;
 EOF
 
 cd ..
 
 wget "https://github.com/probonopd/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
 chmod a+x appimagetool-x86_64.AppImage
+
+echo `pwd`
 
 mkdir out
 cd out 
